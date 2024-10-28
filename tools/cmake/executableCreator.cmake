a5ccb7eecc98e92e685ced46ceeb33b1fdb3b196
Cadons <43477517+Cadons@users.noreply.github.com>
2024-10-28 14:30:03 +0100

    Update executableCreator.cmake

--- tools/cmake/executableCreator.cmake
+++ tools/cmake/executableCreator.cmake
@@ -1,6 +1,6 @@
 # Define executable target
 QT6_ADD_EXECUTABLE(${PROJECT_NAME} ${SOURCES})
 target_link_libraries(${PROJECT_NAME} PRIVATE ${PROJECT_LIBRARIES})
-
+include_directories(${PROJECT_NAME} ${Lib_HDRS})
 # Deployment section for Qt dependencies
 include(tools/cmake/Deploy.cmake)
